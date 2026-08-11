e140df00a2728e7c25d42219bb535b2dc7700e2e
md-pervej <md10pervej@gmail.com>
2026-08-11 22:28:02 +0600

    python problem solving-27 completer

--- Ps-All/problem-solving-27.py
+++ Ps-All/problem-solving-27.py
@@ -1,21 +1,18 @@
 
+# -----27:Python Program to Find LCM-----
 num1=int(input("Enter number-1:"))
 num2=int(input("Enter number-2:"))
 
-def compute_lcm(x,y):
+def computer_lcm(x,y):
     if x>y:
         greater=x
     else:
         greater=y
     while True:
-        if ((greater % x==0) and (greater%y==0)):
+        if ((greater % x==0) and (greater % y==0)):
             lcm=greater
             break
         greater +=1
     return lcm
-
-result=compute_lcm(num1,num2)
+result=computer_lcm(num1,num2)
 print("The L.C.M of ",num1,"and",num2,"is:",result)
-
-
-
--- Test-All/test01.py
+++ Test-All/test01.py
@@ -2,15 +2,18 @@
 
 num1=int(input("Enter number-1:"))
 num2=int(input("Enter number-2:"))
-def compute_hcf(x,y):
 
+def compute_lcm(x,y):
     if x>y:
-        smaller=y
+        greater=x
     else:
-        smaller=x
-    for i in range(1, smaller+1):
-        if((x%i==0) and (y%i==0)):
-            hcf=i
-    return hcf
-result=compute_hcf(num1,num2)
-print("The H.C.F is ",result)
+        greater=y
+
+    while(True):
+        if((greater % x==0) and (greater %y==0)):
+            lcm=greater
+            break
+        greater+=1
+    return lcm
+result=compute_lcm(num1,num2)
+print("The L.C.M of ",num1,"and",num2,"is:",result)
