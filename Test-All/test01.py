300731ddcd7f75ed85833aab4924c4bff7904a49
md-pervej <md10pervej@gmail.com>
2026-08-15 15:14:11 +0600

    python problem solving-35

--- Test-All/test01.py
+++ Test-All/test01.py
@@ -1,16 +1,14 @@
 
 
-def recur_factorial(n):
-    if n==1:
-        return n
-    else:
-        return n* recur_factorial(n-1)
+def convertToBinary(n):
+    if n>1:
+        convertToBinary(n//2)
+        # 5,2,1
+
+    print(n%2,end='')
+    1010
 
 
-num=int(input("Enter a number:"))
-if num<0:
-    print("Factorial does not exists for negative number.")
-elif num==0:
-    print("Factorail of 0 is 1")
-else:
-    print("The factorail of ",recur_factorial(num))
+num=int(input("Enter a number: "))
+
+convertToBinary(num)
