e7010d615e490466c3e7e6eb84657be2b3bdcc37
md-pervej <md10pervej@gmail.com>
2026-08-16 11:46:06 +0600

    python problem solving-16 complete

--- Test-All/test03.py
+++ Test-All/test03.py
@@ -1,10 +1,24 @@
-<<<<<<< HEAD
-=======
 
-def convertToDecimal(n):
-    if n>1:
-        convertToDecimal(n//2)
-    print(n%2,end='')
-num=int(input("Enter a number:"))
-convertToDecimal(num)
->>>>>>> 901d4fba66d1079b6bf7696521607988ca342e80
+X=[
+    [12,7,3],
+    [4,5,6],
+    [7,8,9]
+   ]
+
+Y=[
+    [5,8,1],
+    [6,7,3],
+    [4,5,9],
+]
+result=[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+
+for i in range(len(X)):
+    for j in range(len(X[0])):
+        result[i][j]=X[i][j]+Y[i][j]
+
+for r in result:
+    print(r)
--- Test-All/test04.py
+++ Test-All/test04.py
@@ -1,13 +1,22 @@
-<<<<<<< HEAD
-=======
 
+X=[
+    [12,7,3],
+    [4,5,6],
+    [7,8,9],
+]
 
-def convertToDecimal(n):
-    if n>1:
-        convertToDecimal(n//2)
-    print(n%2,end='')
+Y=[
+    [5,8,1],
+    [6,7,3],
+    [4,5,9],
+]
 
-num=int(input("Enter a number:"))
+result=[
+    [0,0,0]
+    [0,0,0]
+    [0,0,0]
+]
 
-convertToDecimal(num)
->>>>>>> 901d4fba66d1079b6bf7696521607988ca342e80
+for i in range(len(X)):
+    for j in range(len(X[0])):
+        result[i][j]=X[i][j] +Y[i][j]
