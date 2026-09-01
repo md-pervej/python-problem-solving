129c61dd4dcaebb0aafb73c0f1f551b2b2b32aeb
md-pervej <md10pervej@gmail.com>
2026-09-01 14:27:24 +0600

    python problem solving-92 completed

---
+++ Ps-All/problem-solving-92.py
@@ -0,0 +1,17 @@
+
+str1=input("Enter a word-1:")
+str2=input("Enter a word-2:")
+
+str1=str1.lower()
+str2=str2.lower()
+
+if len(str1) == len(str2):
+    sorted_str1=sorted(str1)
+    sorted_str2=sorted(str2)
+
+    if sorted_str1 == sorted_str2:
+        print(str1 + " and "+ str2 +" are anagram")
+    else:
+        print(str1 + " and " + str2 +" are not anagram")
+else:
+    print(str1 + "and " + str2 +"are not anagram")
--- Test-All/test01.py
+++ Test-All/test01.py
@@ -1,9 +1,15 @@
 
-num=int(input("Enter a number to count digit:"))
+str1=input("Enter a word-1:")
+str2=input("Entyer word-2:")
+str1=str1.lower()
+str2=str2.lower()
 
-count=0
-
-while num !=0:
-    num //=10
-    count +=1
-print("Numbers of digit is:",count)
+if (len(str1)==len(str2)):
+    sorted_str1=sorted(str1)
+    sorted_str2=sorted(str2)
+    if (sorted_str1 == sorted_str2):
+        print(str1 +" and "+ str2 +" are anagram")
+    else:
+        print(str1 + "and" + str2 +" is not anagram")
+else:
+    print(str1 + "and"+str2 + " are not anagram")
