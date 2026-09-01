3c429a9f10546d656c616c52d5203daae3e1f1e5
md-pervej <md10pervej@gmail.com>
2026-09-01 14:08:32 +0600

    python problem solving-91 completed

---
+++ Ps-All/problem-solving-91.py
@@ -0,0 +1,13 @@
+
+# -----91:Python Program to Count the Number of Digits Present In a Number-----
+
+num=int(input("Enter a number:"))
+
+# count=0
+#
+# while num !=0:
+#     num //=10
+#     count+=1
+# print("Number of digit is:",count)
+
+print("Number of digit is:",len(str(num)))
